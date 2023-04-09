7ffbbe0a75a5372aabb7218c3f71f2626f2b44aa
Sakshi Patidar <65022281+sakshipatidar@users.noreply.github.com>
2023-04-09 19:26:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const port = 80;
+const port = 5000;
 
 app.use('/static' , express.static('static'));
 app.use(express.urlencoded());
